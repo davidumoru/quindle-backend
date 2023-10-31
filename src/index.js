e6c1dee63b2511facb4c12210b4d923971c7d7a6
David Umoru <umorudavido@gmail.com>
2023-10-31 23:02:41 +0100

    Add routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors")
 
 const connectDB = require("./configs/database");
+const userRoutes = require('./routes/user.routes')
+const matchingRoutes = require('./routes/matching.routes');
 
 const app = express();
 app.use(cors());
@@ -12,6 +14,10 @@ app.use(bodyParser.json());
 
 // connecting to db here
 connectDB(process.env.MONGO_URI);
+
+// Routes
+app.use('/auth', userRoutes)
+app.use('/api', matchingRoutes);
 
 // Defining a route for the root URL ("/")
 app.get("/", (req, res) => {
